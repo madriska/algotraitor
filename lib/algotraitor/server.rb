88b9f96b34ca761b979926bb2fd1ad517bf8e8ba
Brad Ediger <brad.ediger@madriska.com>
2009-08-26 12:48:19 -0500

    Added account.json and buy

--- lib/algotraitor/server.rb
+++ lib/algotraitor/server.rb
@@ -16,10 +16,27 @@ module Algotraitor
     use Rack::Auth::Basic do |username, password|
       market.authenticate(username, password)
     end
-    
+
+    # Current market participant, as authenticated
+    def participant
+      market.participants[env['REMOTE_USER'].to_i]
+    end
+
     get '/stocks.json' do
       content_type 'application/json'
       market.stock_prices.to_json
+    end
+
+    get '/account.json' do
+      content_type 'application/json'
+      {'cash_balance' => participant.cash_balance,
+       'portfolio'    => participant.portfolio}.to_json
+    end
+
+    post '/buy/:symbol/:quantity' do |symbol, quantity|
+      stock = market.stocks[symbol]
+      # body: execution price
+      return participant.buy(stock, quantity)
     end
 
   end
